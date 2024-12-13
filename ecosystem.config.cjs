ac573f72584de9566e3b8e4feeaef91aff934a29
Rushs321 <fyggh566@tutamail.com>
2024-12-13 08:15:38 +0530

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'bandwidth-hero-proxy',
-			script: 'build/index.js',
+			script: 'build/index.ts',
 			node_args:
 				'--expose-gc --max-old-space-size=128 --jitless --no-opt --no-concurrent-recompilation --noexpose_wasm --max-semi-space-size=4',
 		},
